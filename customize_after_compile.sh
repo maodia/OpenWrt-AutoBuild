4b899e1cd0c746193e09aa8ce0ed1e7cd166b918
maodia <maodia@163.com>
2020-01-22 15:10:09 +0800

    Add files via upload

---
+++ customize_after_compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#=================================================
+# Description: custom script excuted after compile
+# Lisence: MIT
+# Author: maoziyuan
+# 
+#=================================================
+echo $0 begin
+#your commend
+#your code
+echo $0 end
