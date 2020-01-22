51875209928ee622fe863e774a1373653de3a8ad
maodia <maodia@163.com>
2020-01-22 15:43:48 +0800

    Update customize_before_compile.sh

--- customize_before_compile.sh
+++ customize_before_compile.sh
@@ -7,5 +7,5 @@
 #=================================================
 echo $0 begin
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.0.200/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.200/g' ./openwrt/package/base-files/files/bin/config_generate
 echo $0 end
